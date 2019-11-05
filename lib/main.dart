5b8f67a10169cc4464d812a4c3b60a606b464e34
AmrAbdelmagid <amrabdelmagid900@gmail.com>
2019-11-06 00:29:17 +0200

    creating project and initial setup

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My DeliMeals',
+      showSemanticsDebugger: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My DeliMeals'),
+      ),
+      body: Center(
+        child: Text('My DeliMeals time'),
+      ),
+    );
+  }
+}
